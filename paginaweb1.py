64d3bc3acbf6fc050bd94165069fcaf9cef1c98f
SofiMedina07 <113081997+SofiMedina07@users.noreply.github.com>
2023-09-24 03:27:58 -0600

    Update paginaweb1.py

--- paginaweb1.py
+++ paginaweb1.py
@@ -8,9 +8,9 @@ openai.api_key = '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'
 # Función que conecta con GPT-4 y devuelve una respuesta a la consulta del usuario
 def obtener_info_inversionistas(inversionista_seleccionado):
     respuesta = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=f"Proporciona información sobre el tipo de {inversionista_seleccionado}.",
-      max_tokens=150  # Ajusta según tus necesidades
+        engine="text-davinci-003",
+        prompt=f"Proporciona información sobre el tipo de {inversionista_seleccionado}.",
+        max_tokens=150  # Ajusta según tus necesidades
     )
     return respuesta.choices[0].text.strip()
     
