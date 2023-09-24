0e7dff9c6c67016a42fc078dd6fb9ebec154b5ed
SofiMedina07 <113081997+SofiMedina07@users.noreply.github.com>
2023-09-24 03:05:56 -0600

    Update paginaweb1.py

--- paginaweb1.py
+++ paginaweb1.py
@@ -18,7 +18,7 @@ def main():
     image = Image.open('Header.jpg')
     
     # Mostrar la imagen
-    st.image(image,caption=None, width=400)
+    st.image(image,caption=None, width=350)
    
     st.header(':red[FINTECH BANORTE]', divider='red')
 
